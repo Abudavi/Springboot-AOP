d9e0a22fab0e504446294074fe6a2aa5e2fdca8a
David MTZD <davidmtzd21512@gmail.com>
2024-10-06 18:09:16 -0600

    se agrego lo que pasa despues

--- src/main/java/com/david/springboot/app/aop/springboot_aop/aop/GreatingAspect.java
+++ src/main/java/com/david/springboot/app/aop/springboot_aop/aop/GreatingAspect.java
@@ -3,6 +3,7 @@ package com.david.springboot.app.aop.springboot_aop.aop;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -15,10 +16,17 @@ public class GreatingAspect {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Before("execution(String com.david.springboot.app.aop.springboot_aop.services.GreatingServices.sayHello(..))")
+    @Before("execution(String com.david.springboot.app.aop.springboot_aop.services.GreatingServices.*(..))")
     public void loggerBefore(JoinPoint jPoint) {
         String method = jPoint.getSignature().getName();
         String args = Arrays.toString(jPoint.getArgs());
         logger.info("Antes " + method + " Con los argumentos " + args);
+    }
+
+    @After("execution(String com.david.springboot.app.aop.springboot_aop.services.GreatingServices.sayHello(..))")
+    public void loggerAfter(JoinPoint jPoint) {
+        String method = jPoint.getSignature().getName();
+        String args = Arrays.toString(jPoint.getArgs());
+        logger.info("Despues " + method + " Con los argumentos " + args);
     }
 }

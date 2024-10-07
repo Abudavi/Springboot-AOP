e0df697a5bcfe99f5d56c82b6e1b9ef5f98bc765
David MTZD <davidmtzd21512@gmail.com>
2024-10-06 22:56:11 -0600

    se agrego otro greatingAspect

---
+++ src/main/java/com/david/springboot/app/aop/springboot_aop/aop/GreatingFooAspect.java
@@ -0,0 +1,35 @@
+package com.david.springboot.app.aop.springboot_aop.aop;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Order(1)
+@Aspect
+@Component
+public class GreatingFooAspect {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Before("execution(String com.david.springboot.app.aop.springboot_aop.services.GreatingServices.*(..))")
+    public void loggerBefore(JoinPoint jPoint) {
+        String method = jPoint.getSignature().getName();
+        String args = Arrays.toString(jPoint.getArgs());
+        logger.info("Antes: " + method + " invocado con los argumentos foo " + args);
+    }
+
+    @After("execution(String com.david.springboot.app.aop.springboot_aop.services.GreatingServices.sayHello(..))")
+    public void loggerAfter(JoinPoint jPoint) {
+        String method = jPoint.getSignature().getName();
+        String args = Arrays.toString(jPoint.getArgs());
+        logger.info("Despues " + method + " Con los parametros foo " + args);
+    }
+
+}

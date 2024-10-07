e47cc97a0f3e6eb5fbbffeb49e7dddf489697785
David MTZD <davidmtzd21512@gmail.com>
2024-10-06 18:02:09 -0600

    se creo la clase greating aspec para que antes de que se ejecute la interface  greating services mande un logger para decir que esta antes de la interface y registre los metodos y argumentros que se le pasan a esta clase

---
+++ src/main/java/com/david/springboot/app/aop/springboot_aop/aop/GreatingAspect.java
@@ -0,0 +1,24 @@
+package com.david.springboot.app.aop.springboot_aop.aop;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class GreatingAspect {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Before("execution(String com.david.springboot.app.aop.springboot_aop.services.GreatingServices.sayHello(..))")
+    public void loggerBefore(JoinPoint jPoint) {
+        String method = jPoint.getSignature().getName();
+        String args = Arrays.toString(jPoint.getArgs());
+        logger.info("Antes " + method + " Con los argumentos " + args);
+    }
+}
